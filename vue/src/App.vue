04bbe26790d8d13820ec583aacdd96c41559c6b4
yinchao <wsyinchao@outlook.com>
2026-02-04 17:43:49 +0800

    about optimized img and envrionment variable

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,47 +2,54 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 
+// console.log(import.meta.env)
+// console.log(import.meta)
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <header>
+        <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+        <div class="wrapper">
+            <HelloWorld msg="You did it!" />
+        </div>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <TheWelcome />
+        <!-- Optimized img by vite -->
+        <!-- added by yinchao, Wed February 04 17:25	2026 -->
+        <!-- <img src="./assets/216.png" alt="a logo">
+        <img src="./assets/logo.svg" alt="logo" width="125" height="125"> -->
+    </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
+    line-height: 1.5;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+    display: block;
+    margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+        margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
 }
 </style>
