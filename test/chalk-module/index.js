bbbbd4f4a83170f040218e499268031bbd047219
yinchao <wsyinchao@outlook.com>
2026-06-10 15:17:47 +0800

    A travel of how to use the module chalk

---
+++ test/chalk-module/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk'
+
+console.log(chalk.blue('Hello world!'))
+console.log(chalk.red('This is a red message.'))
+console.log(chalk.bgBlue.white('This is a message with a blue background and white text.'))
